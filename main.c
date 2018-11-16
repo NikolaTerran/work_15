bf016fc5755512aa46603824f611c792318d0580
Terran Liu <38232187+TianrunLiu@users.noreply.github.com>
2018-11-16 11:45:21 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <fcntl.h>
 
+int main(){
+	printf("parent pid is: %d\n",getpid());
+	int f = fork();
+	if(f){
+		f = fork();
+	}
+	if(!f){
+		int random = open("dev/random",O_RDONLY);
+		int i;
+		read(random,&i,4);
+		i = (i%25)+5;
+		printf("child pid is: %d\n",getpid());
+		printf("i=%d\n",i);
+		printf("gonna sleep for %d seconds\n",sleep(i));
+		printf("child#%d is terminated\n",getpid());
+		exit(i);
+	}
+	int p, status;
+	p = wait(&status);
+	//MACRO();
+	return 0;
+}
